0519a00c66e6292d7f3311decb520c68c625c168
herogeekeduc08-jpg <herogeekeduc08@gmail.com>
2025-10-22 20:10:29 +0000

    22/10 inicio portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,71 @@
 import "./portfolio.css"
 
+const items =[
+   {
+    id: 1,
+    img: "/p1.jpg",
+    title: "Aplicação Blog Full Stack",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 2,
+    img: "/p2.jpg",
+    title: "School Management System",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 3,
+    img: "/p3.jpg",
+    title: "Real-time Chat Application",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 4,
+    img: "/p4.jpg",
+    title: "Social Media Project",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+  {
+    id: 5,
+    img: "/p5.jpg",
+    title: "Animated Portfolio Website",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+    link: "/",
+  },
+]
+
+const ListItem = ({item}) => {
+  return(
+    <div className="pItem">
+      <div className="pImg">
+        <img src={item.img} alt="" />
+      </div>
+      <div className="pText">
+        <h1>{item.title}</h1>
+        <p>{item.desc}</p>
+        <a href={item.link}>
+          <button>View Project</button>
+        </a>
+
+      </div>
+    </div>
+  )
+}
+
 const Portfolio = () => {
   return (
-    <div className='portfolio'>Portfolio</div>
+    <div className='portfolio'>
+      <div className="pList">
+        {items.map(item=>(
+          <ListItem item={item} key={item.id}/>
+
+        ))}
+      </div>
+    </div>
   )
 }
 
