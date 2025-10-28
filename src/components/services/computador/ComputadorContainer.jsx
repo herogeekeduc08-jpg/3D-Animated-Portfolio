ddca906b4943debd728268938ae61a06434e3841
herogeekeduc08-jpg <herogeekeduc08@gmail.com>
2025-10-28 16:19:02 +0000

    28/10 concertado problema com modelo 3D

--- src/components/services/computador/ComputadorContainer.jsx
+++ src/components/services/computador/ComputadorContainer.jsx
@@ -4,18 +4,17 @@ import { Computador } from "./Computador"
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei"
 
 const ComputadorContainer = () => {
-  return (
-    <Canvas>
-        <Suspense fallback="loading...">
-            <Stage environment="night" intensity="0.5">
-                <Computador/>
-            </Stage>
-            <OrbitControls enableZoom={false} autoRotate/>
-            <PerspectiveCamera position ={[-1, 0, 1.8]} zoom={0.8} makeDefault/>
-        </Suspense>
-
-    </Canvas>
-  )
+    return (
+        <Canvas>
+            <Suspense fallback="loading...">
+                <Stage environment="night" intesity="0.5">
+                    <Computador/>
+                </Stage>
+                <OrbitControls enableZoom={false} autoRotate/>
+                <PerspectiveCamera position = {[-1, 0, 1.8]} zoom={0.8} makeDefault/>
+            </Suspense>
+        </Canvas>
+    )
 }
 
 export default ComputadorContainer
