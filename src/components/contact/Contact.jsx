b4d919663154f129dcae1900b19bbcf1b7d04c9a
herogeekeduc08-jpg <herogeekeduc08@gmail.com>
2025-11-07 20:31:20 +0000

    07/11 termino contatos e css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,47 +1,87 @@
-import "./contact.css"
-import emailjs from "@emailjs/browser"
-import { form } from "motion/react-client"
-import { useRef } from "react"
+import "./contact.css";
+import ContactSvg from "./ContactSVG";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import { motion, useInView } from "motion/react";
+
+const listVariant = {
+  initial: {
+    x: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.2,
+    }
+  }
+}
 
 const Contact = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const ref = useRef();
+  const form = useRef();
+
   const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm("YOUR_SERVICE_KEY", "YOUR_TEMPLATE_ID", form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID, 
+        import.meta.env.VITE_TEMPLATE_ID, 
+        form.current, 
+        {
+          publicKey: import.meta.env.VITE_PUBLIC_KEY,
+        }
+      )
       .then(
         () => {
-          console.log('SUCCESS!');
+          setSuccess(true);
+          setError(false);
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          console.log(error)
+          setError(true);
+          setSuccess(false);
         },
       );
   };
 
+  const isInView = useInView(ref, {margin: "-200px"});
+
   return (
-    <div className='contact'>
+    <div className='contact' ref={ref} onSubmit={sendEmail}>
       <div className="cSection">
-        <form>
-          <h1 className="cTitle">Vamos manter em contato</h1>
-          <div className="formItem">
+        <motion.form
+          ref={form} 
+          variants={listVariant} 
+          animate={isInView ? "animate" : "initial"}
+        >
+          <motion.h1 variants={listVariant} className="cTitle">Vamos manter em contato</motion.h1>
+          <motion.div variants={listVariant} className="formItem">
             <label>Nome</label>
-            <input type="text" placeholder="John da Silva" />
-          </div>
-          <div className="formItem">
+            <input type="text" name="user_username" placeholder="John da Silva" />
+          </motion.div>
+          <motion.div variants={listVariant} className="formItem">
             <label>Email</label>
-            <input type="email" placeholder="john@gmail.com" />
-          </div>
-          <div className="formItem">
+            <input type="email" name="user_email" placeholder="john@gmail.com" />
+          </motion.div>
+          <motion.div variants={listVariant} className="formItem">
             <label>Mensagem</label>
-            <textarea rows={10} placeholder="Escreva sua mensagem..."></textarea>
-          </div>
-          <button className="formButton">Enviar</button>
-        </form>
+            <textarea rows={10} name="user_message" placeholder="Escreva sua mensagem..."></textarea>
+          </motion.div>
+          <motion.button variants={listVariant} className="formButton">Enviar</motion.button>
+          {success && <span>Sua mensagem foi enviada com sucesso!</span>}
+          {error && <span>Algo deu errado!</span>}
+        </motion.form>
       </div>
-      <div className="cSection">SVG</div>
+      <div className="cSection">
+        <ContactSvg/>
+      </div>
     </div>
   )
 }
